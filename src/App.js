8ffa488d06dac94fca181bcd38db9ae7417b5578
sohagmahin <sohagmahin@gmail.com>
2020-12-20 23:01:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Radium,{ StyleRoot }from 'radium';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+      { id: 'asfs1', name: "sohag", age: 23 },
+      { id: 'sgfsd2', name: "jahan", age: 29 },
+      { id: 'fgsf3', name: "shamim", age: 33 },
+    ],
+    showPerson: false,
+  };
+
+  deletePerson = (personIndex) => {
+    // const persons = this.state.persons; //copy reference of pointer
+    // const persons = this.state.persons.slice(); //make new reference then copy
+    const persons = [...this.state.persons];//copy data from persons then copy to...
+    persons.splice(personIndex, 1);
+    this.setState({
+      persons: persons
+    })
+  }
+  onChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    console.log(persons[personIndex].name);
+    this.setState({ persons: persons });
+  }
+
+  onPersonToggle = () => {
+    this.setState({ showPerson: !this.state.showPerson });
+    console.log(this.state.showPerson);
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+
+      //Shudo selector
+      ':hover':{
+        backgroundColor: 'lightgreen',
+        color:'white'
+      }
+    }
+
+    let person = null;
+    if (this.state.showPerson) {
+      person = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePerson(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              clickChange={(event) => this.onChangeHandler(event, person.id)}
+            />
+          })}
+        </div>
+      );
+      style.backgroundColor = 'red';
+      style[':hover']={
+        backgroundColor: 'salmon',
+        color:'black'
+      }
+    }
+
+    const classes = [];
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
+    return (
+      <StyleRoot>
+      <div className="App">
+        <button style={style}
+          onClick={this.onPersonToggle}>Toogle Person</button>
+        <p className={classes.join(' ')}>This is persons</p>
+        {person}
+      </div>
+      </StyleRoot>
+    );
+  }
 }
-
-export default App;
+export default Radium(App);
---
+++ src/Person/Person.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Radium from 'radium';
+import './Person.css';
+
+const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)':{
+            width: '450px'
+        }
+    };
+    return (
+        <div className = "Person" style={style}>
+            <p onClick={props.click}>This is {props.name}. And age is {props.age}</p>
+            <p>{props.children}</p>
+            <input type='text' onChange={props.clickChange} value={props.name}></input>
+        </div>
+    );
+}
+
+export default Radium(person);
