71d4200931baaed3bb18ffe1affd4b8bf36a92ef
sohagmahin <sohagmahin@gmail.com>
2021-01-06 20:44:00 +0600

    lesson: 4 video: 8 component update lifecycle

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.module.css'
 
-const person = (props) => {
-    console.log('[person.js] rendering...');
-    return (
-        <div className = {classes.Person}>
-            <p onClick={props.click}>This is {props.name}. And age is {props.age}</p>
-            <p>{props.children}</p>
-            <input type='text' onChange={props.clickChange} value={props.name}></input>
-        </div>
-    );
+class Person extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Person.js] shouldComponent');
+        return true;
+    }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Person.js] getSnapshotBefore update');
+        return { message: 'snapshots' };
+    }
+    componentDidUpdate(prevUpdate, preState, snapshot) {
+        console.log('[Person.js] componentDidUpdate...', snapshot);
+    }
+    render() {
+        console.log('[person.js] rendering...');
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>This is {this.props.name}. And age is {this.props.age}</p>
+                <p>{this.props.children}</p>
+                <input type='text' onChange={this.props.clickChange} value={this.props.name}></input>
+            </div>
+        );
+    }
 }
 
-export default person;
+export default Person;
--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    console.log('[Persons.js] rendering...');
-    return <Person
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        clickChange={(event) => props.clickedChanges(event, person.id)}
-    />
-});
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person, index) => {
+            console.log('[Persons.js] rendering...');
+            return (<Person
+                click={() => this.props.clicked(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                clickChange={(event) => this.props.clickedChanges(event, person.id)}
+            />
+            )
+        });
+    }
+}
 
-export default persons;
+export default Persons;
