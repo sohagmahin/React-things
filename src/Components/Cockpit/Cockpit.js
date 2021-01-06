92686e6bb0f35c2ce2fc604df7b02cc6b57d839c
sohagmahin <sohagmahin@gmail.com>
2021-01-07 01:20:30 +0600

    lesson 4 video 13 cleanup useEffect

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.module.css';
 
 const Cockpit = (props) => {
@@ -6,17 +6,18 @@ const Cockpit = (props) => {
     // if we pass [](empty array) then it call only once/initially
     // if we pass [arg], then it will call when arg passes. useEffect call depent on args
     // if we not pass any array then it will call every time when rerendering the whole app.
-    useEffect(()=>{
-        setTimeout(()=>alert('useEffect alert'),1000);
-        return()=>{
+    useEffect(() => {
+        const timer = setTimeout(() => alert('useEffect alert'), 1000);
+        return () => {
+            clearTimeout(timer);
             console.log('[Cockpit.js] cleanup work in useEffect ')
         }
-        
-    },[props.persons]);
 
-    useEffect(()=>{
+    }, [props.persons]);
+
+    useEffect(() => {
         console.log('[Cockpit.js] useEffect2');
-        return ()=>{
+        return () => {
             console.log('[Cockpit.js] cleanup work in useEffect2');
         }
     });
@@ -24,15 +25,15 @@ const Cockpit = (props) => {
     const assigned = [];
     let btnClass = '';
 
-    if(props.isShowPerson){
+    if (props.isShowPerson) {
         btnClass = classes.Red;
     }
 
     if (props.persons.length <= 2) {
-      assigned.push(classes.Red);
+        assigned.push(classes.Red);
     }
     if (props.persons.length <= 1) {
-      assigned.push(classes.Bold);
+        assigned.push(classes.Bold);
     }
     return (
         <div className={classes.Cockpit}>
