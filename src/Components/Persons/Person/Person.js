7c2646cb0b2f54ebe749cc8dcb825dad33ed26af
sohagmahin <sohagmahin@gmail.com>
2021-01-08 17:14:05 +0600

    lesson 7 video 19: rendering adjacent jsx element

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import classes from './Person.module.css'
+import classes from './Person.module.css';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
     render() {
         console.log('[person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            // <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>This is {this.props.name}. And age is {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input type='text' onChange={this.props.clickChange} value={this.props.name}></input>
-            </div>
+            </Aux>
         );
     }
 }
---
+++ src/hoc/Aux.js
@@ -0,0 +1,6 @@
+//hoc: higher order component
+//Aux: Auxillary
+
+const aux = props => props.children;
+
+export default aux;
