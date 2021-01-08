baa7d076c55845f7d2a34970e10f4f2d957d0ce1
sohagmahin <sohagmahin@gmail.com>
2021-01-08 20:09:28 +0600

    lesson 7 video 26 using PropTypes

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import classes from './Person.module.css';
 import Aux from '../../../hoc/Aux';
 import withClasses from '../../../hoc/withClasses';
+import PropTypes from 'prop-types'
 
 class Person extends Component {
     render() {
@@ -9,12 +10,23 @@ class Person extends Component {
         return (
             // <div className={classes.Person}>
             <Aux>
-                <p onClick={this.props.click}>This is {this.props.name}. And age is {this.props.age}</p>
-                <p>{this.props.children}</p>
+                <p onClick={this.props.click}>
+                    This is {this.props.name}. And age is {this.props.age}
+                </p>
+                <p>
+                    {this.props.children}
+                </p>
                 <input type='text' onChange={this.props.clickChange} value={this.props.name}></input>
             </Aux>
         );
     }
 }
 
-export default withClasses (Person,classes.Person);
+    Person.propTypes = {
+        onClick: PropTypes.func,
+        name: PropTypes.string,
+        age: PropTypes.number,
+        onChange: PropTypes.func,
+}
+
+export default withClasses(Person, classes.Person);
