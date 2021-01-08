66fba4a949c51cb2124e17720abf42c2827cbdc4
sohagmahin <sohagmahin@gmail.com>
2021-01-08 17:19:16 +0600

    lesson 7 video 21: React Fragement [same as Aux]

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.module.css';
 import Aux from '../../../hoc/Aux';
 
@@ -7,11 +7,11 @@ class Person extends Component {
         console.log('[person.js] rendering...');
         return (
             // <div className={classes.Person}>
-            <Aux>
+            <Fragment>
                 <p onClick={this.props.click}>This is {this.props.name}. And age is {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input type='text' onChange={this.props.clickChange} value={this.props.name}></input>
-            </Aux>
+            </Fragment>
         );
     }
 }
